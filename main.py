fbf92ac39f59d38db5b28c88acdb7ca0813115e9
GabrielGomezDlc <89089619+GabrielGomezDlc@users.noreply.github.com>
2025-05-28 05:26:10 -0500

    add new files

---
+++ main.py
@@ -0,0 +1,7 @@
+import subprocess
+
+# Ejecutar el primer script
+subprocess.run(["python", "digitalize_techniques.py"])
+
+# Luego ejecutar el segundo
+subprocess.run(["python", "digitalize_steps.py"])
